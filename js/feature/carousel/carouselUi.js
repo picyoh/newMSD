1848890a8ffbf7894bfc6d535c9d5fbb923dd56e
picyoh <picotyoh@gmail.com>
2022-04-26 17:58:49 +0200

    style: :lipstick: podium
    add podium element to results container

--- js/feature/carousel/carouselUi.js
+++ js/feature/carousel/carouselUi.js
@@ -1,3 +1,34 @@
+// order carousel components
+export const orderCarousel = () => {
+  // check center
+  const carouselCenter = document.querySelector(".carouselCenter");
+  // set base order
+  if (carouselCenter === null) {
+    // left
+    const result5 = document.querySelector("#result5");
+    result5.classList.add("carouselLeft");
+    // center
+    const result0 = document.querySelector("#result0");
+    result0.classList.add("carouselCenter");
+    // right
+    const result1 = document.querySelector("#result1");
+    result1.classList.add("carouselRight");
+    // TODO: add left mouvement
+    // TODO: add right mouvement
+  }
+};
+
+// size background according to %
+const sizeBackground = () => {
+  const results = document.querySelectorAll(".results");
+  results.forEach((result, index) => {
+    // get %
+    const resultScore = result.children[1].innerHTML;
+    result.children[2].style = "height: " + resultScore;
+  });
+};
+
+// append Carousel
 export const appendResults = (resultDatas) => {
   console.log(resultDatas);
   const carousel = `
@@ -8,12 +39,13 @@ export const appendResults = (resultDatas) => {
             .map((result, index) => {
               return `
               ${
-                (index < 2) || (index === 5)
+                index < 2 || index === 5
                   ? `<div id=result${index} class="results">`
                   : `<div id=result${index} class="results hidden">`
               }
                     <img src= ${result.data.src} />
                     <p>${result.score}%</p>
+                    <div class="podium"></div>
                   </div>
                   `;
             })
@@ -22,27 +54,10 @@ export const appendResults = (resultDatas) => {
           </div>
             `;
   content.insertAdjacentHTML("beforeend", carousel);
+  sizeBackground();
   orderCarousel();
 };
 
 export const carouselLeft = () => {};
 
 export const carouselRight = () => {};
-
-export const orderCarousel = () => {
-  // check center
-  const carouselCenter = document.querySelector(".carouselCenter");
-  // set base order
-  if (carouselCenter === null) {
-    // TODO: calc :before height/% and add it to style 
-    // left
-    const result5 = document.querySelector("#result5");
-    result5.classList.add("carouselLeft");
-    // center
-    const result0 = document.querySelector("#result0");
-    result0.classList.add("carouselCenter");
-    // right
-    const result1 = document.querySelector("#result1");
-    result1.classList.add("carouselRight");
-  }
-};
