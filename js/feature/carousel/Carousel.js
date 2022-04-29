b55acca287b82579d0b5b5ed60098c9a77b3668b
picyoh <picotyoh@gmail.com>
2022-04-29 19:28:57 +0200

    refactor: :construction: updateResults
    handle carousel content with updateResults besides appendResults

--- js/feature/carousel/Carousel.js
+++ js/feature/carousel/Carousel.js
@@ -1,64 +1,39 @@
-// order carousel components
-export const orderCarousel = () => {
-  // check center
-  const activeResult = document.querySelector(".activeResult");
-  // set base order
-  if (activeResult === null) {
-    // allocate results
-    const result0 = document.querySelector("#result0");
-    result0.classList.add("activeResult");
-    const result1 = document.querySelector("#result1");
-    result1.classList.add("right");
-    const result2 = document.querySelector("#result2");
-    result2.classList.add("backRight");
-    const result3 = document.querySelector("#result3");
-    result3.classList.add("back");
-    const result4 = document.querySelector("#result4");
-    result4.classList.add("backLeft");
-    const result5 = document.querySelector("#result5");
-    result5.classList.add("left");
-  }
-};
+import { orderCarousel } from "./carouselActions.js";
 
-// TODO: create updateResult to change carouselContainer content
-// append Carousel
+// Carousel structure
 export const appendResults = (resultDatas) => {
   console.log(resultDatas);
   const carousel = `
-          <div class="carousel">
-            <h2>Resultats</h2>
-            <div class="carouselContainer">
-              <button class="carouselBefore"><i class="fa-solid fa-angle-left"></i></button>
-          ${resultDatas
-            .map((result, index) => {
-              return `
-              <div id=result${index} class="results">
-                <div class="imgContainer">
-                  <img src= ${result.data.src} />
-                </div>
-                <p>${result.score}%</p>
-                <div class="matchChart" style="height:${result.score}%"></div>
-              </div>
-                  `;
-            })
-            .join("")}
-              <button class="carouselAfter"><i class="fa-solid fa-angle-right"></i></button>
-            </div>
-          </div>
-            `;
+  <div class="carousel">
+    <h2>Résultats</h2>
+    <div class="carouselContent">
+      <button class="carouselBefore"><i class="fa-solid fa-angle-left"></i></button>
+      <div class="carouselContainer">
+      
+      </div>
+      <button class="carouselAfter"><i class="fa-solid fa-angle-right"></i></button>
+      </div>
+                `;
   content.insertAdjacentHTML("beforeend", carousel);
+  updateResults(resultDatas);
   orderCarousel();
 };
 
-// TODO: add before
-// TODO: add after mouvement
-
-export const carouselBefore = () => {
-  document
-    .querySelector(".carouselBefore")
-    .addEventListener("click", (e) => {
-      
-    });
+// TODO: update content after appendResults
+export const updateResults = (resultDatas) => {
+  const carouselContainer = document.querySelector(".carouselContainer");
+  const carouselContent = resultDatas
+    .map((result, index) => {
+      return `
+        <div id=result${index} class="results">
+          <div class="imgContainer">
+            <img src= ${result.data.src} />
+          </div>
+          <p>${result.score}%</p>
+          <div class="matchChart" style="height:${result.score}%"></div>
+        </div>
+        `;
+    })
+    .join("");
+  carouselContainer.insertAdjacentHTML("afterbegin", carouselContent);
 };
-
-export const carouselAfter = () => {};
--- js/feature/carousel/carouselActions.js
+++ js/feature/carousel/carouselActions.js
@@ -9,21 +9,75 @@ export const setCarousel = () => {
   const results = sortResults(resultDatas, userChoices, questions.length);
   // set carousel
   appendResults(results);
-  // carousel.moveLeft();
+};
+// TODO: checker sans classe juste avec id result0...
+// order carousel components
+export const orderCarousel = () => {
+  // set base results order
+  const result0 = document.querySelector("#result0");
+  result0.classList.add("activeResult");
+  const result1 = document.querySelector("#result1");
+  result1.classList.add("right");
+  const result2 = document.querySelector("#result2");
+  result2.classList.add("backRight");
+  const result3 = document.querySelector("#result3");
+  result3.classList.add("back");
+  const result4 = document.querySelector("#result4");
+  result4.classList.add("backLeft");
+  const result5 = document.querySelector("#result5");
+  result5.classList.add("left");
+  carouselBefore();
+  carouselAfter();
 };
 
-export const moveTop = () => {
-    const topLeft = document.querySelector("#topLeft");
-    topLeft.addEventListener("click", (e)=>{
+// TODO: check bug rotatebefore
+const carouselBefore = () => {
+  document.querySelector(".carouselBefore").addEventListener("click", (e) => {
+    console.log("left");
+    rotate("left");
+  });
+};
 
-      const carouselContainer = document.querySelector(".carouselContainer");
+const carouselAfter = () => {
+  document.querySelector(".carouselAfter").addEventListener("click", (e) => {
+    console.log("right");
+    rotate("right");
+  });
+};
 
-      const top = document.querySelector("#top");
-      const topRight = document.querySelector("#topRight");
-      const botRight = document.querySelector("#botRight");
-    })
+// TODO: carousel mouvement
+const rotate = (direction) => {
+  let degree = 0;
+
+  switch (direction) {
+    //   case "up":
+    //     return degree =
+    case "right":
+      degree = -60;
+      break;
+    //   case "down":
+    case "left":
+      degree = 60;
+      break;
+    default:
+      degree;
   }
-
-export const moveDown = () => {
-
-}
+  console.log(degree);
+  // move carousel
+  const carousel = document.querySelector(".carouselContainer");
+  carousel.style.transform = "rotateY(" + degree + "deg)";
+  // move items
+  const items = document.querySelectorAll(".results");
+  items.forEach((item, index) => {
+    // add a rotation to
+    item.style.transform =
+      "translateX(150%) translateY(25%) rotateY(" +
+      60 * index +
+      "deg) translateZ(250px) rotateY(-" +
+      60 * index +
+      "deg) rotateY(" +
+      -degree +
+      "deg)";
+  });
+  // TODO: change class? check on codepen
+};
