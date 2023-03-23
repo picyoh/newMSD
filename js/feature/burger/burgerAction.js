624c2da4ebf13b2201036587bbf7913a45c2169a
picyoh <picotyoh@gmail.com>
2023-03-23 16:24:45 +0100

    lastCommitQDev

--- js/feature/burger/burgerAction.js
+++ js/feature/burger/burgerAction.js
@@ -3,14 +3,11 @@ export const handleBurger = () => {
     e.preventDefault();
     e.stopPropagation();
     document.querySelector(".menu-container").classList.remove("hidden");
+    setCurve();
     transformBurger();
     handleClose();
   });
 };
-
-const transformBurger = () => {
-    // document.querySelector('burger-icon')
-}
 
 export const handleClose = () => {
     document.querySelector('#menu-burger').addEventListener('click', (e)=> {
@@ -19,4 +16,32 @@ export const handleClose = () => {
         document.querySelector(".menu-container").classList.add("hidden");
         handleBurger();
     })
+}
+
+const transformBurger = () => {
+  // document.querySelector('burger-icon')
+}
+
+const setCurve = () => {
+  let i = 0;
+  document.querySelector('.up-header').classList.add('flatten');
+  document.querySelector('footer').classList.add('flatten');
+  const elements = document.querySelectorAll('.menu-element');
+  let eLength = elements.length -1;
+  
+  console.log(eLength);
+
+  elements.forEach((e) => {
+    if(e.classList.contains("hidden")){
+     eLength--;
+    }else {
+      if(i == 0){
+        e.classList.add("top-curve");
+      }
+      if (i == eLength){
+        e.classList.add("bottom-curve");
+      }
+      i++;
+    }
+  })
 }
