60ecd701c7f6abab7f0cb84be5d223302540f904
picyoh <60223835+picyoh@users.noreply.github.com>
2024-09-25 17:28:01 +0200

    feat: :sparkles: add checkbox on answers
    add checkbox mecanism in the answer generation

--- js/feature/questions/Question.js
+++ js/feature/questions/Question.js
@@ -1,5 +1,6 @@
 export const appendFirstQuestion = () => {
-  const firstValue= JSON.parse(sessionStorage.getItem('questionsItem'))[0].question;
+  const firstValue = JSON.parse(sessionStorage.getItem("questionsItem"))[0]
+    .question;
   const first = `
     <section id="content">
       <div id="questions">
@@ -13,7 +14,7 @@ export const appendFirstQuestion = () => {
     </section>
       `;
   document.querySelector("#main").insertAdjacentHTML("beforeend", first);
-  sessionStorage.setItem('currentIndex', '1');
+  sessionStorage.setItem("currentIndex", "1");
 };
 
 export const appendQuestion = (currentIndex) => {
@@ -21,14 +22,14 @@ export const appendQuestion = (currentIndex) => {
     currentIndex
   ];
   const userData = JSON.parse(sessionStorage.getItem("userData"));
-  console.log(currentQuestion)
+  //console.log(currentQuestion);
   const button = `
             <div id="question${currentIndex}" class="question ariseQuestion">
             ${
               currentQuestion.question === undefined
                 ? currentQuestion.qMulti
                     .map((element) => {
-                      console.log(element)
+                      //console.log(element);
                       if (userData.indexOf(element.parent) > -1) {
                         return `<h2>${element.question}</h2>`;
                       }
@@ -39,6 +40,31 @@ export const appendQuestion = (currentIndex) => {
             <div class="answerGroup">
               ${currentQuestion.choices
                 .map((choice) => {
+                  //console.log(choice);
+                  if (choice.titles) {
+                    let checkboxes = '';
+                    choice.titles.forEach((element)=>{
+                      checkboxes +=
+                      `
+                      <input type="checkbox" id="${element.tag}" name="${element.tag}" />
+                      <label for="${element.tag}">${element.title}</label>
+                      `
+                    });
+                    checkboxes += 
+                    `
+                    <label for="other_${choice.name}">Autres : </label>
+                    <input type="text" id="other_${choice.name}" name="other_${choice.name}" />
+                    <button id='checkbox_validation> class='validation'>Valider</button>
+                    `
+                    //console.log(checkboxes);
+                    return `
+                    <div class="answer">
+                      <div id='${choice.name}'>
+                      ${checkboxes}
+                      </div>
+                    </div>
+                    `;
+                  };
                   if (choice.src !== undefined) {
                     return `
                   <div class="answer">
@@ -47,7 +73,8 @@ export const appendQuestion = (currentIndex) => {
                   }
                   if (
                     userData.indexOf(choice.parent) > -1 ||
-                    choice.parent === undefined || choice.parent === "" 
+                    choice.parent === undefined ||
+                    choice.parent === ""
                   ) {
                     return `<button id=${choice.tag.replace(
                       " ",
@@ -64,8 +91,8 @@ export const appendQuestion = (currentIndex) => {
 };
 
 export const animationRemoveClass = (toRemove) => {
-const arisingQuestion = document.querySelector('#question' + toRemove);
-setTimeout(function() {
-  arisingQuestion.classList.remove('ariseQuestion');
-}, 1)
-}
+  const arisingQuestion = document.querySelector("#question" + toRemove);
+  setTimeout(function () {
+    arisingQuestion.classList.remove("ariseQuestion");
+  }, 1);
+};
